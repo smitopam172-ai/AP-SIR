935504cd7088c73d3683e4310f42be1a73d74f27
smitopam172-ai <smitopam172@gmail.com>
2026-04-21 02:21:47 +0530

    Create p_2.c

---
+++ p_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+  int year;
+printf("enter year");
+scanf("%d",&year);
+if (year%4==0 || year %400==0){
+printf("leap year");
+}
+else if (year %100==0){
+printf("0");}
+return 0;}
