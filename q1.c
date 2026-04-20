7221bf1f84fb03bb67282ea2bf273338a525c03e
smitopam172-ai <smitopam172@gmail.com>
2026-04-21 02:16:06 +0530

    Add program to check if a number is even or odd

---
+++ q1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+  int main(){
+  int n;
+  printf("enter interger");
+  scanf("%d",&n);
+  if(n%2==0){
+printf("number is even");
+  }
+else if (n%2!=0)
+{
+printf("number is odd");}
+return 0;}
+  
