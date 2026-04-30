b3b3f7edff0541eaaf45d1ec25c52cc47c3e7de6
smitopam172-ai <smitopam172@gmail.com>
2026-05-01 01:43:50 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,32 +1,22 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main() {
     int num;
 
     printf("Enter a number (up to 5 digits): ");
     scanf("%d", &num);
-    if (num < 0) {
-        num = -num;
-    }
 
-    if (num >= 0 && num <= 9) {
-        printf("The number has 1 digit.\n");
-    } 
-    else if (num >= 10 && num <= 99) {
-        printf("The number has 2 digits.\n");
-    } 
-    else if (num >= 100 && num <= 999) {
-        printf("The number has 3 digits.\n");
-    } 
-    else if (num >= 1000 && num <= 9999) {
-        printf("The number has 4 digits.\n");
-    } 
-    else if (num >= 10000 && num <= 99999) {
-        printf("The number has 5 digits.\n");
-    } 
-    else {
-        printf("The number exceeds 5 digits.\n");
-    }
+    // Handle negative numbers
+    if (num < 0) num = -num;
+
+    // Nested Ternary Operators
+    (num <= 9)     ? printf("The number has 1 digit.\n") :
+    (num <= 99)    ? printf("The number has 2 digits.\n") :
+    (num <= 999)   ? printf("The number has 3 digits.\n") :
+    (num <= 9999)  ? printf("The number has 4 digits.\n") :
+    (num <= 99999) ? printf("The number has 5 digits.\n") :
+                     printf("The number exceeds 5 digits.\n");
 
     return 0;
 }
+}*/
