944c353bc808d233cc233b861244fca095cfde64
smitopam172-ai <smitopam172@gmail.com>
2026-05-01 01:37:43 +0530

    Update q1.c

--- q1.c
+++ q1.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
   int main(){
   int n;
   printf("enter interger");
@@ -9,5 +9,12 @@ printf("number is even");
 else if (n%2!=0)
 {
 printf("number is odd");}
-return 0;}
+return 0;}*/
+#include<stdio.h>
+int main(){
+    int n=5
+    ;
+    (n%2==0)?printf("even"):printf("odd");
+    return 0;
+}
   
